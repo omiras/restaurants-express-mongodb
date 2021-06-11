96447558c7109b673c363c2b8e1801a021344613
oskytar <oscar.miras@gmail.com>
2021-06-11 08:25:49 +0200

    requeriment 1 implemented

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const express = require('express');
 
-// Usaremos esta variable global para poder acceder a la base de datos desde cualquier parte de la aplicación
-let _db;
-
 const app = express();
 
 const uri = "mongodb+srv://root:root@cluster0.lo8dg.mongodb.net/?retryWrites=true&w=majority";
@@ -14,15 +11,26 @@ const client = new MongoClient(uri, {
 
 
 // Usamos una función de callback que se ejecuta cuando la conexión a la base de datos se ha efectuado (y es el punto seguro donde podemos empezar a hacer cosas, como por ejemlo, levantar el servior Express en el puerto 3000)
-client.connect(function(err, db) {
+client.connect(function(err) {
     if (err) throw err;
     console.log('Conectado a la base de datos correctamente.')
-    
-    // Objeto Db de MongoDB; para poder acceder a las bases de datos, colecciones, etc.
-    _db = db;
     app.listen(3000)
   });
 
-app.get('/', (req, res) => {
-    res.render('index.ejs')
+
+app.get('/', async (req, res) => {
+    const db = client.db('sample_restaurants');
+    const restaurants = db.collection('restaurants');
+
+    if (!req.query) {
+        res.render('index.ejs', {
+            restaurants: []
+        })
+    }
+    const results = await restaurants.find({restaurant_id: req.query.restaurant_id}).toArray();
+    
+    res.render('index.ejs', {
+        restaurants: results
+    })
 })
+

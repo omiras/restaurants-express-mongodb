7591d79598bbf7859f92a7711dba1d3fe4904456
oskytar <oscar.miras@gmail.com>
2021-06-11 08:08:54 +0200

    uri

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let _db;
 
 const app = express();
 
-const uri = "mongodb+srv://root:root@cluster0.lo8dg.mongodb.net/?retryWrites=true&w=majority";
+const uri = "TU-CONNECTIONSTRING-AQUI";
 const MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
